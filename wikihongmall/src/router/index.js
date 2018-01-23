e9cfb4ab40330dbb36e8047f505316bbabc16802
wikihong <hjpChinash@163.com>
2018-01-23 15:59:21 +0800

    路由嵌套--0123

--- wikihongmall/src/router/index.js
+++ wikihongmall/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from './../views/goodsList.vue'
+import Title from '@/views/title.vue'
+import Image from '@/views/image.vue'
 
 Vue.use(Router)
 
@@ -8,9 +10,21 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/goods/:goosId/user/:userName',
+      path: '/goods',
       name: 'GoodsList',
-      component: GoodsList
+      component: GoodsList,
+      children: [
+        {
+          path: 'title',
+          name: 'title',
+          component: Title
+        },
+        {
+          path: 'image',
+          name: 'image',
+          component: Image
+        }
+      ]
     }
   ]
 })
--- wikihongmall/src/views/goodsList.vue
+++ wikihongmall/src/views/goodsList.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>这个是商品列表页面</div>
-  <!-- <span>{{$routes.params.goodId}}</span> -->
+  <div>
+    <div>这个是商品列表页面</div>
+    <!-- <span>{{$routes.params.goodId}}</span> -->
+    <router-link to="/goods/title">显示商品标题</router-link>
+    <router-link to="/goods/image">显示商品图片</router-link>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
