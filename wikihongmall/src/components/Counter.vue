f4be7cb5a74ac297598316f08a865969d51ce66f
wikihong <hjpChinash@163.com>
2018-01-23 14:40:32 +0800

    props父传子-0123

---
+++ wikihongmall/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="increase">+</button>
+    <button @click="decrement">-</button>
+    <p><span>{{ myNum }}</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['num'],
+  data () {
+    return {
+      myNum: this.num
+    }
+  },
+  methods: {
+    increase (myNum) {
+      let _this = this
+      _this.myNum++
+    },
+    decrement (myNum) {
+      this.myNum--
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
